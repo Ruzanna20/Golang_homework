d2a08339848fd282b9cd2070799a2c6c349d9141
Ruzanna20 <ruzannasargsyan.tt219-1@polytechnic.am>
2025-07-04 00:01:23 +0400

    Update 54.go

--- 54/54.go
+++ 54/54.go
@@ -18,20 +18,21 @@ func main() {
 		val,_ := strconv.Atoi(line)
 		if val >= 100 && val <= 999{
 			fmt.Println(val)
+		
+			m := val % 10
+			t := (val / 10) % 10
+			h := val / 100 
+			max := m
+			if t > max {
+				max = t
+			}
+			if h > max {
+				max = h
+			}
+			fmt.Println(max)
 		} else{
-			fmt.Println("Eranish tiv che:")
+			fmt.Println("Eranish tiv che:",val)
 		}
 
-	m := val % 10
-	t := (val / 10) % 10
-	h := val / 100 
-	max := m
-	if t > max {
-		max = t
-	}
-	if h > max {
-		max = h
-	}
-	fmt.Println(max)
 }
 }
