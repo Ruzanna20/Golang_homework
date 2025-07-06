53ec7154f147fd20f2f641a8c89bd101133e20c9
Ruzanna Sargsyan <ruzannasargsyan.tt219-1@polytechnic.am>
2025-07-06 21:20:30 +0400

    282

--- 282/main.go
+++ 282/main.go
@@ -1,2 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"inputproject/main1"
+)
+
+func CheckValue(arr []int) []int {
+	var Y []int
+	for _, val := range arr {
+		if val != 0 {
+			Y = append(Y, val)
+		}
+	}
+	return Y
+}
+
+func main() {
+	X := main1.InputArray(5)
+	Y := CheckValue(X)
+
+	fmt.Println("Y: ",Y)
+}
