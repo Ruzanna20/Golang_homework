68b5cb33e434af8c84d054d31ea792e0451fd091
Ruzanna Sargsyan <ruzannasargsyan.tt219-1@polytechnic.am>
2025-07-10 23:49:09 +0400

    Update DLL

--- DLL/main.go
+++ DLL/main.go
@@ -67,7 +67,7 @@ func (dll *DLL) deleteFromDLL(data int) {
 	if dll.head.data == data && dll.head.next == nil {
 		dll.head = nil
 		dll.tail = nil
-		return 
+		return
 	}
 
 	current := dll.head
@@ -76,12 +76,66 @@ func (dll *DLL) deleteFromDLL(data int) {
 			current.next = current.next.next
 			current.next.prev = current
 		} else if current.next.data == data && current.next.next == nil {
-			current.next = nil 
+			current.next = nil
 			dll.tail = current
 			return
 		}
 		current = current.next
 	}
+}
+
+func (dll *DLL) countOfNodeInDLL() int {
+	var count int
+
+	current := dll.head
+	for current != nil {
+		count++
+		current = current.next
+	}
+	return count
+}
+
+func (dll *DLL) checkNegativeDataInDLL() bool {
+	var negativedata bool
+
+	current := dll.head
+	for current != nil {
+		if current.data < 0 {
+			negativedata = true
+			break
+		}
+		current = current.next
+	}
+	return negativedata
+}
+
+func (dll *DLL) sumOfAllDataInDLL() int {
+	var sum int
+
+	current := dll.head
+	for current != nil {
+		sum += current.data
+		current = current.next
+	}
+	return sum
+}
+
+func (dll *DLL) sumAtOddIndexesInDLL() int {
+	var sum int
+	var arrofodd []int
+
+	current := dll.head
+	for current != nil {
+		arrofodd = append(arrofodd, current.data)
+		current = current.next
+	}
+
+	for i, val := range arrofodd {
+		if i%2 == 1 {
+			sum += val
+		}
+	}
+	return sum
 }
 
 func (dll *DLL) print() {
@@ -111,4 +165,16 @@ func main() {
 	dll.print()
 	dll.deleteFromDLL(70)
 	dll.print()
+
+	count := dll.countOfNodeInDLL()
+	fmt.Println("Count of node in the DLL:", count)
+
+	negativedata := dll.checkNegativeDataInDLL()
+	fmt.Println("DLL has the negative data:", negativedata)
+
+	sumofalldata := dll.sumOfAllDataInDLL()
+	fmt.Println("Sum of the all data in the DLL:", sumofalldata)
+
+	sumofoddindexes := dll.sumAtOddIndexesInDLL()
+	fmt.Println("Sum of odd index nodes in DLL:", sumofoddindexes)
 }
