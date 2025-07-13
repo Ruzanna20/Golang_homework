cf01f409b075193e865956df23f63afec63cc0d3
Ruzanna Sargsyan <ruzannasargsyan.tt219-1@polytechnic.am>
2025-07-13 14:58:48 +0400

    Update interfaces

--- Interfaces/interface.go
+++ Interfaces/interface.go
@@ -1,13 +1,16 @@
 package interfaces
 
 type LL interface{
-	InsertAtBegin(data int) 
+	InsertAtBegin(data int)
 	InsertAtEnd(data int)
-	Delete(data int)
-	Print()
-	CountOfNode() 
-	CheckNegativeData() 
-	SumOfAllData()
-	SumAtOddIndexes()
+	Delete(data int) bool
+
+	CountOfNode() int
+	CheckNegativeData() bool
+	SumOfAllData() int
+	SumAtOddIndexes() int
+
+	Print() 
 }
+
 
