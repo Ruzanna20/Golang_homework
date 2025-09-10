3d14713febc7a233fe9d8c8ff997f24d94abeeb4
Ruzanna20 <ruzannasargsyan.tt219-1@polytechnic.am>
2025-09-11 01:04:50 +0400

    Update main.go

--- MCP_Server/main.go
+++ MCP_Server/main.go
@@ -3,27 +3,49 @@ package main
 import (
 	"database/sql"
 	"fmt"
-
-	_ "github.com/denisenkom/go-mssqldb"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	fmt.Println("MCP Server-y sksum e ashxatanqy...")
+	fmt.Println("MCP Server starts working...")
 
-	connStr := "server=localhost\\SQLEXPRESS;database=DB;trusted_connection=yes"
+	connStr := "host=localhost port=5432 user=postgres password=12345 dbname=mydb sslmode=disable"
 
-	db, err := sql.Open("sqlserver", connStr)
+	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		fmt.Println("Sxal kapakcum:", err)
+		fmt.Println("Incorrect connection:", err)
 	}
 
 	defer db.Close()
 
 	err = db.Ping()
 	if err != nil {
-		fmt.Println("Ping sxal:", err)
+		fmt.Println("Ping error:", err)
 		return
 	}
 
-	fmt.Println("db-in normal kapvecinq:")
+	_,err = db.Exec("INSERT INTO users(name,email) VALUES($1,$2)","Ruzanna","ruzs3145@gmail.com")
+	if err != nil {
+		fmt.Println("Insert error:",err)
+	}
+
+	rows, err := db.Query("SELECT id, name, email FROM users")
+	if err != nil {
+		fmt.Println("Select error",err)
+		return 
+	}
+
+	defer rows.Close()
+
+	for rows.Next(){
+		var id int
+		var name,email string
+		err = rows.Scan(&id,&name,&email)
+		if err != nil {
+			fmt.Println("Scan error",err)
+		}
+		fmt.Println(id,name,email)
+	}
+
+	fmt.Println("Connected to DB successfully.")
 }
