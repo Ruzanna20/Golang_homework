89853de7c33e28b5a56c45a223cdf05027232806
Ruzanna20 <ruzannasargsyan.tt219-1@polytechnic.am>
2025-09-14 00:45:19 +0400

    Add files via upload

--- main.go
+++ main.go
@@ -1,100 +1,56 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io"
-    "net/http"
+	"database/sql"
+	"fmt"
+	"log"
+	"mcp_sql_server_/repository"
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
-type Title struct {
-    Ru string `json:"ru"`
-    En string `json:"en"`
-    Am string `json:"am"`
-}
-type Company struct {
-    Domain_id           int    `json:"domain_id"`
-    Profile_image       string `json:"profile_image"`
-    Id                  int    `json:"id"`
-    Title               Title  `json:"title"`
-    Is_unlimited        int    `json:"is_unlimited"`
-    Is_verified         bool   `json:"is_verified"`
-    Slug                string `json:"slug"`
-    IsEmptyProfileImage bool   `json:"isEmptyProfileImage"`
-}
-
-type Category struct {
-    Code  string `json:"code"`
-    Id    int    `json:"id"`
-    Title Title  `json:"title"`
-}
-
-type Job struct {
-    Title           Title    `json:"title"`
-    Deadline        string   `json:"deadline"`
-    CompaniesStruct Company  `json:"companiesStruct"`
-    Category        Category `json:"category"`
-    Slug            Title    `json:"slug"`
-    Status          int      `json:"status"`
-    Saved_job_id    bool     `json:"saved_job_id"`
-    Like_id         bool     `json:"like_id"`
-    Is_featured     bool     `json:"is_featured"`
-    LikeCount       int      `json:"likeCount"`
-    Is_new          bool     `json:"is_new"`
-    ItemType        int      `json:"itemType"`
-    Is_following    bool     `json:"is_following"`
-}
-
-type PageProps struct {
-    Jobs       []Job `json:"jobs"`
-    TotalCount int   `json:"totalCount"`
-}
-
-type ApiResponse struct {
-    PageProps PageProps `json:"pageProps"`
-}
-
-func fetchJobs() ([]Job, error) {
-    const url = "https://staff.am/_next/data/QqNfKEDjEeO01hJlkXWg_/am/jobs.json"
-    req, err := http.NewRequest("GET", url, nil)
-    if err != nil {
-        return nil, err
-    }
-
-    req.Header.Set("User-Agent", "Mozilla/5.0")
-    req.Header.Set("Accept", "application/json")
-
-    client := &http.Client{}
-    resp, err := client.Do(req)
-    if err != nil {
-        return nil, err
-    }
-
-    defer resp.Body.Close()
-
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        return nil, err
-    }
-
-    var response ApiResponse
-    err = json.Unmarshal(body, &response)
-    if err != nil {
-        return nil, err
-    }
-    return response.PageProps.Jobs, nil
-}
-
 func main() {
-    jobs, err := fetchJobs()
-    if err != nil {
-        fmt.Println("Error", err)
-        return
-    }
+	fmt.Println("MCP Server starts working...")
 
-    fmt.Printf("Found %d jobs:\n", len(jobs))
-    for i, job := range jobs {
-        fmt.Printf("%d. %s - %s\n", i+1, job.Title.Am, job.CompaniesStruct.Title.Am)
+	server := "localhost"
+	port := 1433
+	user := "SA"
+	password := "Ruzanna04!"
+	database := "TravelDB"
 
-    }
-}
+	connStr := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s",
+	server,user,password,port,database)
+
+	db,err := sql.Open("sqlserver",connStr)
+	if err != nil {
+		log.Fatal("Incorrect connection:",err.Error())
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("Ping error:",err)
+	}
+	fmt.Println("Connected to DB successfully.")
+
+
+	results,err := repository.GetContactOfUsers(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _,res := range results {
+			fmt.Println("Phone of User:",res["FirstName"],"-",res["Phone"])		
+		}
+	
+	fmt.Println("")
+
+	users,err := repository.GetAllUsers(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _,u := range users {
+		fmt.Print("User:",u,"\n")
+	}
+
+	}
